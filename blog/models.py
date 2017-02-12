1eaebf7f3381ece9d1e0a0b0ead8f74665ad2177
0sw4l <ioswxd@gmail.com>
2017-02-12 12:53:19 -0500

    leo

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class post(models.Model):
+	author = models.ForeignKey('auth.user')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(auto_now_add=True)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	def _str_(self):
+	    return self.title
